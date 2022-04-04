163cc8c6c4ba359e7197605c020c12d794b17856
Toky <466350259@qq.com>
2022-04-04 22:08:44 +0800

    到BM10

---
+++ 牛客必刷101/BM10.java
@@ -0,0 +1,77 @@
+package 牛客必刷101;
+//两个链表的第一个公共结点
+//题目中的信息:
+//●两个链表含有公共结点或没有，有公共结点则返回第1公共结点指针
+//●单链表，无循环
+//●没有公共节点返回空
+//方法1:双指针长度比较法(推荐使用)
+//具体做法:
+//如果两个链表有公共节点，那么它们后半部分都是相同的，我们要找的也就是后半部分的第一个节点，只要遍历两个指
+//针同时抵达第1个相同的节点， 我们就找到了它。
+//●step1:单独的遍历两个链表，得到各自的长度。
+//●step2:求得两链表的长度差n,其中较长的链表的指针从头先走n步。
+//●step3: 两链表指针同步向后遍历，遇到第1个相同的节点就是第1个公共结点。
+public class BM10 {
+    public class Solution1 {
+        public int ListLenth(ListNode pHead){  //计算链表长度的函数
+            ListNode p = pHead;
+            int n = 0;
+            while(p != null){
+                n++;
+                p = p.next;
+            }
+            return n;
+        }
+        public ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
+            int p1 = ListLenth(pHead1);
+            int p2 = ListLenth(pHead2);
+            if(p1 >= p2){  //当链表1更长时，链表1指针先走p1-p2步
+                int n = p1 - p2;
+                for(int i = 0; i < n; i++){
+                    pHead1 = pHead1.next;
+                }
+                //两个链表同时移动，直到有公共结点时停下
+                while((pHead1 != null) && (pHead2 != null) && (pHead1 != pHead2)){
+                    pHead1 = pHead1.next;
+                    pHead2 = pHead2.next;
+                }
+            }
+            else{  //反之，则链表2先行p2-p1步
+                int n = p2 - p1;
+                for(int i = 0; i < n; i++){
+                    pHead2 = pHead2.next;
+                }
+                //两个链表同时移动，直到有公共结点时停下
+                while((pHead1 != null) && (pHead2 != null) && (pHead1 != pHead2)){
+                    pHead1 = pHead1.next;
+                    pHead2 = pHead2.next;
+                }
+            }
+            return pHead1;//随便返回一个公共节点
+        }
+    }
+
+
+    //由上种方法长度差的思路，不同上述一个指针先走,另1个指针后走， 仅需将两个链表连在一起，两个指针同步走。易知
+    //将两个链表连在一起长度都相等，对于遍历两个链表的两个指针，公共部分走的步数是一样的， 非公共部分因都走了两
+    //个链表，因此也是相同的，所以绕了一圈，第一个相同的结点便是第一个公共结点。
+    //●step1: 判断链表情况，其中有一个为空，则不能有公共结点,返回null。
+    //●step2:两个链表都从表头开始同步依次遍历。
+    //●step3:不需要物理上将两个链表连在一起，仅需指针在一个链表的尾部时直接跳到另一个链表的头部。
+    //●step4: 根据上述说法，第一个相同的结点便是第一个公共结点。
+    public class Solution2 {
+        public ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
+            //其中有一个为空，则不能有公共结点，返回null
+            if(pHead1 == null || pHead2 == null)
+                return null;
+            ListNode p1 = pHead1;
+            ListNode p2 = pHead2;
+            while(p1 != p2){ //相当于遍历两次两个链表所有值
+                p1 = p1 == null ? pHead2 : p1.next;
+                p2 = p2 == null ? pHead1 : p2.next;
+            }
+            return p1;
+        }
+
+    }
+}
---
+++ 牛客必刷101/BM9.java
@@ -0,0 +1,63 @@
+package 牛客必刷101;
+//删除链表的倒数第n个节点
+//题目的主要信息:
+//●给定一个链表，要删除链表倒数第n个节点，并返回链表的头
+//●题目保证链表长度- -定大于等于n
+//方法1: 双指针(推荐使用)
+//具体做法:
+//我们无法逆序遍历链表，就很难得到链表的倒数第n个元素，那我们可以试试反过来考虑，如果当前我们处于倒数第n
+//的位置上，即距离链表尾的距离是n,那我们假设双指针指向这两个位置，二者同步向前移动，当前面个指针到了链表
+//头的时候，两个指针之间的距离还是n。虽然我们没有办法让指针逆向移动，但是我们刚刚这个思路却可以正向实施:
+//●step1:给链表添加一个表头，处理删掉第一个元素时比较方便。
+//●step2: 准备一个快指针，在链表上先走n步。
+//●step 3:准备慢指针指向原始链表头，代表当前元素，前序节点指向添加的表头，这样两个指针之间相距就是一-直
+//都是n。
+//●step4:快慢指针同步移动，当快指针到达链表尾部的时候，慢指针正好到了倒数n个元素的位置。
+
+public class BM9 {
+    public class Solution1 {
+
+        public ListNode removeNthFromEnd (ListNode head, int n) {
+            ListNode res = new ListNode(-1); //添加表头
+            res.next = head;
+            ListNode cur = head; //当前节点
+            ListNode pre = res; //前序节点
+            ListNode fast = head;
+            while(n != 0){ //快指针先行n步
+                fast = fast.next;
+                n--;
+            }
+            //快慢指针同步，快指针到达末尾，慢指针就到了倒数第n个位置
+            while(fast != null){
+                fast = fast.next;
+                pre = cur;
+                cur = cur.next;
+            }
+            pre.next = cur.next; //删除该位置的节点
+            return res.next; //返回去掉头
+        }
+    }
+
+
+    // 长度统计法（思路扩展）
+    public class Solution2 {
+        public ListNode removeNthFromEnd (ListNode head, int n) {
+            int length = 0; //记录链表长度
+            ListNode res = new ListNode(-1); //添加表头
+            res.next = head;
+            ListNode cur = head; //当前节点
+            ListNode pre = res; //前序节点
+            while(cur != null){ //找到链表长度
+                length++;
+                cur = cur.next;
+            }
+            cur = head; //回到头部
+            for(int i = 0; i < length - n; i++){ //从头遍历找到倒数第n个位置
+                pre = cur;
+                cur = cur.next;
+            }
+            pre.next = cur.next; //删去倒数第n个节点
+            return res.next; //返回去掉头节点
+        }
+    }
+}
